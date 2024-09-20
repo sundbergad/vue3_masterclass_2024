96bc384f8b5780540ea546a8befaa7fc7e777ed1
sundbergad <anders@itan.se>
2024-09-20 11:23:40 +0200

    Initial setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports
+  = {
+    extends: [
+      'plugin:vue/vue3-essential',
+      'eslint:recommended',
+      '@vue/typescript/recommended',
+      '@vue/prettier',
+      '@vue/eslint-config-typescript',
+      '@vue/eslint-config-prettier/skip-formatting'
+    ],
+    parserOptions: {
+      ecmaVersion: latest,
+    },
+    
+  }
